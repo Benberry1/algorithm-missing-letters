4e62dd5b3ae90be8641a419b5c7caf1ed4f3a4e5
Ben Berry <ben.berry@developersinstitute.co.nz>
2022-08-13 11:47:18 +1200

    feat: implement missingLetters happy path solution

--- solution.js
+++ solution.js
@@ -1,5 +1,17 @@
 const missingLetters = (str) => {
-  return "Setup complete";
+  let missingLetter;
+
+  let charCode = str.charCodeAt(0);
+
+  for (let i = 1; i < str.length; i++) {
+    if (str.charCodeAt([i]) === charCode + 1) {
+      charCode++;
+    } else {
+      missingLetter = String.fromCharCode(charCode + 1);
+    }
+  }
+
+  return missingLetter;
 };
 
 module.exports = { missingLetters };
--- solution.test.js
+++ solution.test.js
@@ -2,12 +2,15 @@
 // The string comprises of consecutive letters of the alphabet
 // When a string of letters is passed to the function, it is checked for the missing letter
 // The missing letter is returned || if no missing letter then undefined is returned.
+// A better way to handle this would be to return a string "There is no missing letter!"
+// However, the challenge specifcally wants an undefined return.
 
 const { missingLetters } = require("./solution");
 
 describe("Missing letters function", () => {
   test("A string is returned", () => {
-    const result = missingLetters("abc");
+    // This check needs to pass an parameter with a missing letter otherwise undefined is returned as per acceptence criteria
+    const result = missingLetters("abd");
     expect(typeof result === "string").toBe(true);
   });
 
